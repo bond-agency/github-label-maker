d9345212b248b1cb024a809a7a137f7d15166042
Roope Merikukka <roope.merikukka@bond.fi>
2016-06-01 14:21:54 +0300

    Bug fix & print enhancements.

--- labelmaker.rb
+++ labelmaker.rb
@@ -28,10 +28,12 @@ while (!action.eql? "Q") || (!action.eql? "q") do
     checkIfRepositoryExists(repo_full_name)
     createLabels(repo_full_name)
   elsif (action.eql? "A") || (action.eql? "a")
-    repos = client.repos()
+    repos = $client.repos()
     for repo in repos
       repo = repo.rels[:self].get.data
+      puts "Creating labels for: #{$bold}#{repo.full_name}#{$unbold}"
       createLabels(repo.full_name)
+      printRuler()
     end
   elsif (action.eql? "Q") || (action.eql? "q")
     #Quit.
